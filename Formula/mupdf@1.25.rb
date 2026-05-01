dbcc26410cde57e80ee08ad9365bb616251a064b
jfw7 <jonathan@jonathanwallmusic.com>
2026-04-30 22:14:35 -0400

    Strip Python bindings — only mutool needed, avoids llvm libclang TypeError

--- Formula/mupdf@1.25.rb
+++ Formula/mupdf@1.25.rb
@@ -5,9 +5,7 @@ class MupdfAT125 < Formula
   sha256 "5a51d8bd5ed690d3c8bf82b3c7c3f1cf5f9dde40887a36e3b5aa78a7e3ccd1bb"
   license "AGPL-3.0-or-later"
 
-  depends_on "llvm" => :build
   depends_on "pkgconf" => :build
-  depends_on "swig" => :build
   depends_on "freetype"
   depends_on "gumbo-parser"
   depends_on "harfbuzz"
@@ -17,7 +15,6 @@ class MupdfAT125 < Formula
   depends_on "mujs"
   depends_on "openjpeg"
   depends_on "openssl@3"
-  depends_on "python@3.13"
   depends_on "tesseract"
 
   uses_from_macos "zlib"
@@ -35,20 +32,15 @@ class MupdfAT125 < Formula
     keep = %w[extract lcms2]
     (buildpath/"thirdparty").each_child { |path| rm_r(path) if keep.exclude? path.basename.to_s }
 
-    site_packages = Language::Python.site_packages("python3.13")
-    ENV.prepend_path "PYTHONPATH", Formula["llvm"].opt_prefix/site_packages
-
     args = %W[
       build=release
       shared=yes
       tesseract=yes
       verbose=yes
       prefix=#{prefix}
-      pydir=#{prefix/site_packages}
       CC=#{ENV.cc}
       USE_SYSTEM_LIBS=yes
       USE_SYSTEM_MUJS=yes
-      VENV_FLAG=
     ]
 
     if OS.mac?
@@ -64,18 +56,9 @@ class MupdfAT125 < Formula
         args << "SYS_#{argname}_LIBS=#{Utils.safe_popen_read("pkgconf", "--libs", libname).strip}"
         args << "HAVE_SYS_#{argname}=yes"
       end
-
-      inreplace "Makefile" do |s|
-        s.gsub!(/^\s*ln -sf libmupdf/, "#\\0")
-        s.gsub! "libmupdfcpp.$(SO)", "libmupdfcpp.so"
-        s.gsub! "_mupdf.$(SO)", "_mupdf.so"
-      end
-
-      ENV.cxx11
     end
 
     system "make", "install", *args
-    system "make", "install-shared-python", *args
 
     bin.install_symlink bin/"mutool" => "mudraw"
     man1.install_symlink man1/"mutool.1" => "mudraw.1"
